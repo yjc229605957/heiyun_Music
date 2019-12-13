4ccf5debe705d8719f374e3f4638f4b262d52e9b
yjc229605957 <1198472052@qq.com>
2019-12-13 11:48:33 +0800

    项目起步-router/element-ui整合

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+//导入路由
+import router from './router/router';
+//导入饿了么ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// 注册一下
+Vue.use(ElementUI);
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  //将router挂载到vue实例中
+  router,
 }).$mount('#app')
---
+++ src/router/router.js
@@ -0,0 +1,13 @@
+//导入vue
+import Vue from 'vue';
+//导包
+import VueRouter from 'vue-router';
+//注册一下
+Vue.use(VueRouter)
+//路由规则
+const routes = []
+//创建router实例
+const router = new VueRouter({
+  routes
+});
+export default router
